10ddbfd179b9a4eff09b0b27adaba8193e1079e8
GA2204 <almas.gheyas@stir.ac.uk>
2026-03-05 17:26:04 +0000

    Add installation script for dartR dependencies
    Install Bioconductor and key CRAN packages for dartR.

---
+++ install.R
@@ -0,0 +1,15 @@
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+
+# Install Bioconductor manager
+install.packages("BiocManager")
+
+# Bioconductor packages needed for dartR dependencies
+BiocManager::install(c("snpStats", "SNPRelate"), update = FALSE, ask = FALSE)
+
+# Install key CRAN packages
+install.packages(c(
+  "dartR.base",
+  "dartRverse",
+  "directlabels",
+  "tidyverse"
+), dependencies = TRUE)
